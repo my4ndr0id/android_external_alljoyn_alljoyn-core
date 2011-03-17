441bf7a9810ed609f0cf52d11e90ce415ec143e8
Greg Burns <gburns@quicinc.com>
2011-03-17 14:06:16 -0700

    Added a new bus listener callback that is called AFTER a session joiner has succesfully joined a session. This is the first time that the session can actually be used, and specifically is the first time that GetSessionFd() can be called to obtain the socket file descriptor from a raw session.

--- src/BusAttachment.cc
+++ src/BusAttachment.cc
@@ -1036,7 +1036,7 @@ bool BusAttachment::Internal::CallAcceptListeners(const char* sessionName, Sessi
     list<BusListener*>::iterator it = listeners.begin();
     bool isAccepted = false;
     while (it != listeners.end()) {
-        bool answer = (*it)->AcceptSession(sessionName, id, joiner, qos);
+        bool answer = (*it)->AcceptSessionJoiner(sessionName, id, joiner, qos);
         if (answer) {
             isAccepted = true;
         }
@@ -1044,6 +1044,18 @@ bool BusAttachment::Internal::CallAcceptListeners(const char* sessionName, Sessi
     }
     listenersLock.Unlock();
     return isAccepted;
+}
+
+void BusAttachment::Internal::CallJoinedListeners(const char* sessionName, SessionId id, const char* joiner)
+{
+    /* Call all listeners. */
+    listenersLock.Lock();
+    list<BusListener*>::iterator it = listeners.begin();
+    while (it != listeners.end()) {
+        (*it)->SessionJoined(sessionName, id, joiner);
+        ++it;
+    }
+    listenersLock.Unlock();
 }
 
 }
